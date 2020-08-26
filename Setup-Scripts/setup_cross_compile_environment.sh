70430288e61cf33cc2e6f4e88e4b01ea12d36b76
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-08-27 00:03:30 +0200

    Fixed issue in rsync calls for cross compile environment

--- Setup-Scripts/setup_cross_compile_environment.sh
+++ Setup-Scripts/setup_cross_compile_environment.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$1" != "" ]; then
+    if [ "$1" != "update" ]; then
       IP=$1
+  else
+      IP="192.168.178.48"
+  fi
 else
       IP="192.168.178.48"
 fi
@@ -25,8 +29,9 @@ if [ "$UPDATE" != "1" ]; then
       git clone https://github.com/raspberrypi/tools.git ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools
 fi
 
- sshpass -p $PASS rsync -r -P --links pi@$IP:/lib ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
+
  sshpass -p $PASS rsync -r -P --links pi@$IP:/usr ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
  sshpass -p $PASS rsync -r -P --links pi@$IP:/bin ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
  sshpass -p $PASS rsync -r -P --links pi@$IP:/opt ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
  sshpass -p $PASS rsync -r -P --links pi@$IP:/var ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
+ sshpass -p $PASS rsync -r -P --links pi@$IP:/lib ../../Cyborg-1.5-Oled-Eye-RaspberryPi-Cross-Compile-Env/raspberrypi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/
