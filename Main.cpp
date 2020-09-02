50885326274bc9fbf346df4274b5941d759fb40f
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-09-02 23:23:42 +0200

    Implemented timeScale Feature to engine

--- Main.cpp
+++ Main.cpp
@@ -15,7 +15,11 @@ void sigint_handler(int sig) {
 unsigned long  millis(){
     auto time = std::chrono::high_resolution_clock::now().time_since_epoch();
     return std::chrono::duration_cast<std::chrono::milliseconds>(time).count() -  startTime;
-
+}
+float timeScale = 1;
+float refValue = 1000/60;
+float time_scale(){
+    return timeScale;
 }
 
 int main(int argc, char* argv[]) {
@@ -23,8 +27,9 @@ int main(int argc, char* argv[]) {
 
     startTime = millis();
 
-    int targetFPS = 60;
-    int targetFrameTime = 1000/targetFPS;
+    int targetFPS = 30;
+    //int targetFPS = 10; // for easier debugging
+    float targetFrameTime = 1000/targetFPS;
 
     printf("Target FPS %d\n", targetFPS);
 
@@ -37,13 +42,21 @@ int main(int argc, char* argv[]) {
     while(!cancel){
         unsigned long now = millis();
 
-        cancel = cyberEye.loop(now);
+        bool cancel_app = cyberEye.loop(now);
+
+        cancel = cancel || cancel_app;
 
         unsigned long after = millis();
 
-        unsigned long waitTime = targetFrameTime-(after-now);
+        unsigned long loopTime = (after-now);
 
+        unsigned long waitTime = (loopTime > targetFrameTime ? 0 : targetFrameTime-loopTime);
+        //printf("Wait time %d \n", waitTime);
         usleep(waitTime * 1000);
+
+        timeScale = (float) (loopTime + waitTime) / refValue;
+
+        printf("Time Scale:  %f  -  Wait Time: %d\n", time_scale(), waitTime);
     }
 
 }
