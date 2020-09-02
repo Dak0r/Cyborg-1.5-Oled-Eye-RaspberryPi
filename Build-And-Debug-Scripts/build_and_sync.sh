ea7700b344c5c9a1583c91814670f5cb525698ae
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-09-02 22:57:12 +0200

    Added build and sync script

---
+++ Build-And-Debug-Scripts/build_and_sync.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo $0
+echo $1
+
+if [ "$1" != "" ]; then
+      IP=$1
+else
+      IP="192.168.178.48"
+fi
+if [ "$2" != "" ]; then
+      PASS=$2
+else
+      PASS="raspberry"
+fi
+
+./build.sh
+
+./utils/sync_build.sh $IP $PASS
